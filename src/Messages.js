7439a055b9cbd8c6f298d8f450721db15c49e1ff
Dhruv-97 <dhruvp@umass.edu>
2024-04-03 19:21:04 -0400

    Front end display for messages
    Created front end for messages page

--- src/Messages.js
+++ src/Messages.js
@@ -1,10 +1,65 @@
+import "./Messages.css"
 import NavBar from "./NavBar";
+export const Conversation = () =>{
+    return(
+        <div className="conversation">
+            <span className="conversationName">Skeeyee</span>
+        </div>
+    )
 
+}
+export const Message = ({own}) => {
+    return (
+    <div className={own ? "message own": "message" }>
+        <div className="messageTop">
+            <p className="messageText">Skeeeeeyyeeeeeeee</p>
+        </div>
+    <div className="messageBottom">Sent moments ago</div>
+    </div>
+    )
+}
 const Messages = () => {
     return (
         <>
             <NavBar />
             <h1>Messages</h1>
+            <div className="messenger">
+                <div className="chatMenu">
+                    <div className="chatMenuWrapper">
+                        <input placeholder="Find Friends" className="chatMenuInput"></input>
+                        <Conversation/>
+                        <Conversation/>
+                        <Conversation/>
+                        <Conversation/>
+                        <Conversation/>
+                        <Conversation/>
+                        <Conversation/>
+                    </div>
+                </div>
+                <div className="chatBox"> 
+                    <div className="chatBoxWrapper">
+                        <div className="chatBoxTop">
+                            <Message/>
+                            <Message own={true}/>
+                            <Message/>
+                            <Message/>
+                            <Message own={true}/>
+                            <Message/>                            <Message/>
+                            <Message own={true}/>
+                            <Message/>
+                        </div>
+                        <div className="chatBoxBottom">
+                            <textarea className="chatMessageInput" placeholder="send message..."></textarea>
+                            <button className="chatSubmitButton">Send</button>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="chatOnline">
+                    <div className="chatOnlineWrapper">
+                        online
+                    </div>
+                </div> */}
+            </div>
         </>
     )
 }
