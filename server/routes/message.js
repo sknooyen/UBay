4b1162654bbc3bb65c318ed4bb60c4ad9c1637a9
Griffin Laverty <89666030+GriffinLaverty@users.noreply.github.com>
2024-04-21 17:12:34 -0400

    Initial commit

---
+++ server/routes/message.js
@@ -0,0 +1,48 @@
+const Message = require("../models/Message")
+const { route } = require("./auth");
+const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken")
+
+const router = require("express").Router();
+
+let collection = db.collection("Chat Logs")
+
+//Starts a chat between two users
+router.newChat("/", async (req, res)=> {
+   try{
+        let newChat = {
+            user1 : req.body.user1,
+            user2 : req.body.user2,
+            message_objects: []
+        }
+        let result = await collection.insertOne(newChat)
+        res.send(result).status(204);
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error starting new chat")
+    }
+
+    //adds a new message object to the list
+    router.newMessage("/", async (req, res)=> {
+        try{
+            let message_object = {
+                sender: req.body.sender,
+                message: req.body.message,
+                time: req.body.time
+            }
+
+            let query = {user1:req.body.user1, user2:req.body.user2}
+            let update = {
+                    $push: {
+                        message_objects: message_object
+                    }
+            }
+            let result = await collection.updateOne(query,update);
+        
+        }catch(err){
+            console.error("error occured: ", err)
+        }
+        
+    })
+
+
+})
