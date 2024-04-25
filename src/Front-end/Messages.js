85b87f3d082b33c1dbeca5fb21a07dbe7e547832
Dhruv-97 <dhruvp@umass.edu>
2024-04-25 18:26:27 -0400

    Front-End messages view complete
    Message component needs to be integrated with the backend. Searching for individuals to chat with still must be implemented.

--- src/Front-end/Messages.js
+++ src/Front-end/Messages.js
@@ -1,67 +1,165 @@
 import "./Messages.css"
+import React, { useState, useRef, useEffect } from "react";
+import ReactDOM from 'react-dom'
 import NavBar from "./NavBar";
-export const Conversation = () =>{
-    return(
-        <div className="conversation">
-            <span className="conversationName">Sample_User</span>
-        </div>
-    )
+import { pageTheme } from "./util";
+import { Typography, Box, TextareaAutosize ,Container, Grid, Paper, TextField, ThemeProvider, List, ListItem, Checkbox, ListItemText, Slider, Input, MenuItem, Select, Stack, Button } from '@mui/material';
+import axios from 'axios';
 
-}
-export const Message = ({own}) => {
-    return (
-    <div className={own ? "message own": "message" }>
-        <div className="messageTop">
-            <p className="messageText">This is a message</p>
-        </div>
-    <div className="messageBottom">Sent moments ago</div>
-    </div>
-    )
-}
+
+// }
+// const handleNewChat = () =>{
+//     axios.newChat('http://localhost:8000/api/messages',data).then(res => {
+//         // clear the form fields
+//         // notify user that their listing was posted
+//       }).catch(error => {
+//         console.error('Error posting data:', error);
+// })}
+
+//sample messages only sent by the current user
+export let listOfMessages = []
+
+export const USERLIST = ["Buyer1", "Seller1", "Buyer2", "Selling2"]
+
+const messages = [
+    { text: "Hi there!", isSender: false },
+    { text: "Hello! How are you?", isSender: true },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+    { text: "I'm good, thanks for asking. What about you?", isSender: false },
+
+
+];
+
+
+
 const Messages = () => {
-    return (
-        <>
-            <NavBar />
-            <h1>Messages</h1>
-            <div className="messenger">
-                <div className="chatMenu">
-                    <div className="chatMenuWrapper">
-                        <input placeholder="Find Friends" className="chatMenuInput"></input>
-                        <Conversation/>
-                        <Conversation/>
-                        <Conversation/>
-                        <Conversation/>
-                        <Conversation/>
-                        <Conversation/>
-                        <Conversation/>
-                    </div>
-                </div>
-                <div className="chatBox"> 
-                    <div className="chatBoxWrapper">
-                        <div className="chatBoxTop">
-                            <Message/>
-                            <Message own={true}/>
-                            <Message/>
-                            <Message/>
-                            <Message own={true}/>
-                            <Message/>                            <Message/>
-                            <Message own={true}/>
-                            <Message/>
-                        </div>
-                        <div className="chatBoxBottom">
-                            <textarea className="chatMessageInput" placeholder="send message..."></textarea>
-                            <button className="chatSubmitButton">Send</button>
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="chatOnline">
-                    <div className="chatOnlineWrapper">
-                        online
-                    </div>
-                </div> */}
+    // State to store the user input
+    const [inputValue, setInputValue] = useState('');
+    const [convo, setConvo] = useState(USERLIST[0])
+    const [allMessages, setAllMessages] = useState(messages)
+    const [friends, setFriendsList] = useState(USERLIST)
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+      }, [allMessages]);
+
+    const handleMessageInput = (message) => () => {
+        setAllMessages([...allMessages, message])
+        //TODO: Set allMessages to what the database returns
+    }
+    // Function to handle changes in the text field
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleConvoChange = (name) => {
+        setConvo(name)
+        //TODO: Change the messages associated with conversation
+    }
+    const handleSend = (message) => {
+        if (inputValue.trim() !== '') {
+          
+            // Add new message to the messages array
+          setAllMessages([...allMessages, message]);
+          //TODO: Send message to the database
+          
+          // Clear the input field after sending a message
+          setInputValue('');
+        } 
+      };
+
+    return (    
+        <ThemeProvider theme = {pageTheme}>
+            <div>
+                <NavBar />
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item xs={3}>
+                            <Paper>
+                            <Box sx={{
+                                        maxHeight: 500,
+                                        overflowY: 'auto',
+                                        margin: 'auto'}}>
+                                <Typography align="center" variant="h6">Conversations</Typography>
+                                <TextField sx={{ m: 2, width: '25ch' }} id="filled-basic" label="Search" variant="filled" />
+
+                                <Stack align="center" direction={"column"} spacing={2}>
+                                    {USERLIST.map(option => 
+                                        <ListItem align="center" key={option}>
+                                            <Button align="center" onClick={() => {handleConvoChange(option);}}>
+                                                {option}
+                                            </Button>
+                                        </ListItem>
+                                    )}
+                                </Stack>
+                            </Box>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <Paper>
+                                <Typography align="center" variant="h6">{convo}</Typography>
+                                <Box sx={{
+                                        maxHeight: 500,
+                                        overflowY: 'auto',
+                                        margin: 'auto'}}>
+                                    <List>
+                                        {allMessages.map((message, index) => (
+                                            <ListItem key={index} sx={{
+                                                display: 'flex',
+                                                justifyContent: message.isSender ? 'flex-end' : 'flex-start',
+                                                padding: '10px'
+                                            }}>
+                                            <Typography variant="body1">{message.text}</Typography>
+                                        </ListItem>
+                                        ))}
+                                        <div ref={messagesEndRef} />
+                                    </List>
+                                </Box>
+                            </Paper>
+                        </Grid>
+                            <Box sx={{ position: 'fixed', bottom: "8%", width: '50%', padding: 1, right:'19%' }}>
+                                <TextField
+                                    fullWidth
+                                    multiline
+                                    label="Send a message..."
+                                    variant="outlined"
+                                    value={inputValue}
+                                    onChange={handleInputChange}
+                                />
+                            </Box>
+                            <Button variant="contained" sx={{ position: 'fixed', bottom: '10%', width: '8%', padding: 1, right:'10%' }} 
+                                    onClick={() => { handleSend({text: inputValue, isSender: true}) }}>
+                                    Send
+                            </Button>
+                    </Grid>
+                </Container>
             </div>
-        </>
+        </ThemeProvider>
     )
 }
+
 
  export default Messages
